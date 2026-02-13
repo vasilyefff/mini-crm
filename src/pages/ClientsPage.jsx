57e14fe9f3b328fb4ee0dcbe34c39cea1d4a9621
vasilyefff <vasilyeff_k@mail.ru>
2026-02-13 08:59:08 +0300

    Add clients/deals CRUD and status colors

--- src/pages/ClientsPage.jsx
+++ src/pages/ClientsPage.jsx
@@ -1,18 +1,57 @@
 import { clients } from "../data/clients";
+import { useState } from "react";
 
 export default function ClientsPage() {
+	const [clientsList, setClientsList] = useState(clients);
+	const [newClientName, setNewClientName] = useState("");
+
+	const handleDeleteClient = (id) => {
+		setClientsList((prev) => prev.filter((client) => client.id !== id));
+	};
+
+	const handleAddClient = () => {
+		if (!newClientName) return;
+
+		const newClient = {
+			id: Date.now(),
+			name: newClientName,
+			email: "new@test.com",
+			status: "active",
+		};
+
+		setClientsList((prev) => [...prev, newClient]);
+		setNewClientName("");
+	};
+
+
 	return (
 		<div>
 			<h2>Clients</h2>
+			<input
+				placeholder="Client name"
+				value={newClientName}
+				onChange={(e) => setNewClientName(e.target.value)}
+			/>
 
-			<ul>
-				{clients.map((client) => (
-					<li key={client.id}>
-						{client.name} — {client.email}
-					</li>
-				))}
-			</ul>
+			<button onClick={handleAddClient}>
+				Add client
+			</button>
+
+
+			{clientsList.length === 0 ? (<p>No clients yet</p>) : (
+				<ul>
+					{clientsList.map((client) => (
+						<li key={client.id}>
+							{client.name} — {client.email}
+							<button onClick={() => handleDeleteClient(client.id)}>
+								Delete
+							</button>
+						</li>
+					))}
+				</ul>
+			)}
 		</div>
+
 	);
 
 }
--- src/pages/DealsPage.jsx
+++ src/pages/DealsPage.jsx
@@ -1,18 +1,39 @@
 import { deals } from "../data/deals"
+import { useState } from "react"
+
+const dealStatusColor = {
+	"new": "blue",
+	"in-progress": "orange",
+	"done": "green"
+}
 
 export default function DealsPage() {
+	const [dealsList, setDealsList] = useState(deals);
+
+	const handleDeleteDeal = (id) => {
+		setDealsList((prev) => prev.filter((deal) => deal.id !== id));
+	};
+
+
 	return (
 		<div>
 			<h2>Deals</h2>
 
-			<ul>
-				{deals.map((deal) => (
-					<li key={deal.id}>
-						{deal.title} — ${deal.amount} — {deal.status}
-					</li>
-				))}
-			</ul>
-		</div>
+			{dealsList.length === 0 ? (<p>No deals yet</p>) : (
+				<ul>
+					{dealsList.map((deal) => (
+						<li key={deal.id}>
+							{deal.title} — ${deal.amount} — <span style={{ color: dealStatusColor[deal.status] || "black" }}>{deal.status}</span>
+							<button onClick={() => handleDeleteDeal(deal.id)}>
+								Delete
+							</button>
+						</li>
+					))}
+				</ul>
+			)
+			}
+
+		</div >
 	);
 
 }
